87c7317388b8833b5df4c0a4a0df07adc71a8c8a
wertyyyy12 <43356692+wertyyyy12@users.noreply.github.com>
2020-10-24 14:01:57 -0700

    teachers start sessions, student can "join" a session

--- server.js
+++ server.js
@@ -3,14 +3,44 @@ const WebSocket = require("ws");
 const server = new WebSocket.Server({ port: 3000 });
  
 var clients = [];
+var sessions = {};
+var students = {};
 
 function on_connection(client) {
   console.log("New connection!");
   clients.push(client);
 
   client.on("message", function onMessage(msg) {
-      broadcast(msg);
-  });
+      var messageObject = JSON.parse(msg); 
+      console.log(messageObject);
+      msgType = messageObject.type;
+      console.log(msgType);
+      if (msgType == "Teacher") {
+          var currentSID = messageObject.sessionID;
+          var currentGroupSize = messageObject.groupSize; 
+          sessions[currentSID] = [currentGroupSize];
+          console.log(sessions);
+      }
+
+      if (msgType == "Student") {
+        var currentSID = messageObject.sessionID;
+        if (sessions[currentSID]) {
+            sessions[currentSID].push(messageObject.studentName); //push the student name to the session id
+        }
+        else {
+            console.log("This session doesn't exist!");
+        }
+        console.log(sessions);
+      }
+
+      if (msgType == "End") {
+          broadcast("/" + messageObject.sessionID); // "/" header to message = end session
+      }
+
+      if (msgType == "Get") {
+        client.send("|" + JSON.stringify(sessions[messageObject.sessionID]));
+      }
+});
 }
 
 function broadcast(msg) {

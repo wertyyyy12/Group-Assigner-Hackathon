4610c4fba0d78150c4719d1d6d502f3170f032dc
wertyyyy12 <43356692+wertyyyy12@users.noreply.github.com>
2020-10-24 18:12:46 -0700

    kjhf

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = new WebSocket.Server({ port: 3000 });
 var clients = [];
 var sessions = {};
 var prefrences = {};
+var currentGroupSize;
 
 function on_connection(client) {
   console.log("New connection");
@@ -16,25 +17,25 @@ function on_connection(client) {
         var client = clients[i];
 
         if (client.readyState == WebSocket.OPEN) { 
-            client.send(msg);
+                client.send(msg);
+            }
         }
     }
-}
 
   client.on("message", function onMessage(msg) {
       var messageObject = JSON.parse(msg); 
       msgType = messageObject.type;
       if (msgType == "Teacher") {
           var currentSID = messageObject.sessionID;
-          var currentGroupSize = messageObject.groupSize; 
-          sessions[currentSID] = [currentGroupSize];
+          currentGroupSize = messageObject.groupSize; 
+          sessions[currentSID]["groupSize"] = currentGroupSize;
       }
 
       if (msgType == "Student") {
         var currentSID = messageObject.sessionID;
         if (sessions[currentSID]) {
             sessions[currentSID].push(messageObject.studentName); //push the student name to the session id
-            broadcast("Update prefrences list|" + currentSID);
+            broadcast("Update prefrence list|" + currentSID);
         }
         else {
             console.log("This session doesn't exist!");
@@ -50,8 +51,24 @@ function on_connection(client) {
           //prepare prefrences dictionary with key values of sessions
           prefrences = sessions;
           for (const [sID, prefs] of Object.entries(prefrences)) {
-            prefrences[sID] = "";
+            prefrences[sID] = {};
           }
+          console.log()
+      }
+
+      if (msgType == "StudentEnd") {
+          var thatStudentsID = messageObject.studentData[1];
+          var thatStudentName = messageObject.studentData[0];
+          var thatStudentPrefrences = messageObject.studentData;
+          thatStudentPrefrences.shift();
+          thatStudentPrefrences.shift();
+          //shift twice to delete student's ID and name from prefrences
+          prefrences[thatStudentsID][thatStudentName] = thatStudentPrefrences;
+          console.log("sessions: ");
+          console.log(sessions);
+          prefrences[thatStudentsID]["groupSize"] = sessions[thatStudentsID][0]; //buggy for sure but who gives a f--
+
+          console.log(prefrences);
       }
 
       if (msgType == "Get") {
